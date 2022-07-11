34b643d53ba172eaed71fa6a4bd1b715cf9c3546
EduSanSou <103901211+EduSanSou@users.noreply.github.com>
2022-07-11 15:56:42 -0300

    Create i2c_uart.ino

---
+++ i2c_uart.ino
@@ -0,0 +1,32 @@
+#include <Wire.h>
+
+void setup()
+{
+  Wire.begin(0x0A);                
+  Wire.onReceive(receiveEvent);
+  digitalWrite(SCL, LOW);
+  digitalWrite(SDA, LOW);
+  Serial.begin(115200);          
+}
+
+void loop() {
+  if(Serial.available()){
+   String mov_string = Serial.readString(); //arduino armazena string de movimento
+  }
+}
+
+
+void receiveEvent(int numBytes)
+{
+  while (1 < Wire.available())
+  { 
+    char td = Wire.read();
+    Serial.print(td);        
+  }
+  int valor = Wire.read();    
+  Serial.println("");
+  Serial.println(valor, HEX);   
+  Serial.println(valor, BIN);
+  Serial.println(valor, OCT);
+  Serial.println(valor);       
+}
